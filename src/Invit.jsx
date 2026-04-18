753d06055a433f84994300152f12d6decbb3b64e
ASTRAARDENCY <astraardency@gmail.com>
2026-04-18 08:16:32 +0530

    Update Invit.jsx

--- src/Invit.jsx
+++ src/Invit.jsx
@@ -8,21 +8,21 @@ function Invit({ isEntered }) {
                 <div className="divider stagger stagger-1"></div>
                 
                 <p className="stagger stagger-2">
-                    The paint is dry, the boxes are unpacked, and we are finally ready to turn our new house into a home. We would be absolutely thrilled if you would join us to celebrate!
+                    if you would join us to celebrate!
                 </p>
 
                 <div className="details stagger stagger-3">
                     <div className="detail-block">
                         <h3>When</h3>
-                        <p>Saturday, Oct 24<br />6:00 PM </p>
+                        <p>-------, ---, --<br />6:00 AM </p>
                     </div>
                     <div className="detail-block">
                         <h3>Where</h3>
-                        <p>123 New Home Ave<br />Cityville, ST</p>
+                        <p>-- --- ---<br /-----, --</p>
                     </div>
                 </div>
 
-                <a href="mailto:you@email.com" className="rsvp-btn stagger stagger-4">RSVP Now</a>
+                <a href="10.931781, 79.648595" className="rsvp-btn stagger stagger-4">Location</a>
             </div>
         </div>
     )
