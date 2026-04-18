9ef10c76a338c368ed95e062c9fa7ae55f8421e3
ASTRAARDENCY <astraardency@gmail.com>
2026-04-18 08:30:39 +0530

    Update Invit.jsx

--- src/Invit.jsx
+++ src/Invit.jsx
@@ -8,21 +8,26 @@ function Invit({ isEntered }) {
                 <div className="divider stagger stagger-1"></div>
                 
                 <p className="stagger stagger-2">
-                    if you would join us to celebrate!
+                    The paint is dry, the boxes are unpacked, and we are finally ready to turn our new house into a home. We would be absolutely thrilled if you would join us to celebrate!
                 </p>
 
                 <div className="details stagger stagger-3">
                     <div className="detail-block">
                         <h3>When</h3>
-                        <p>-------, ---, --<br />6:00 AM </p>
+                        <p>Saturday, Oct 24<br />6:00 PM </p>
                     </div>
                     <div className="detail-block">
                         <h3>Where</h3>
-                        <p>-- --- ---<br /-----, --</p>
+                        <p>123 New Home Ave<br />Cityville, ST</p>
                     </div>
                 </div>
 
-                <a href="10.931781, 79.648595" className="rsvp-btn stagger stagger-4">Location</a>
+                <a 
+                    href="https://www.google.com/maps/search/?api=1&query=10.931781,79.648595" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className="rsvp-btn stagger stagger-4">
+                    Get Directions</a>
             </div>
         </div>
     )
